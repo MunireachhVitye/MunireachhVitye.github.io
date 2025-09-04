3de6006fd1f3bc18b762e6cef615da247dcc5483
MunireachhVitye <mvityea@gmail.com>
2025-09-04 09:04:28 +0700

    Updated content to use PWA of vas-comms

--- vityea.js
+++ vityea.js
@@ -20,20 +20,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
-function updateInfo(){
-    const x = new XMLHttpRequest();
-    x.withCredentials = true;
-    x.open("GET", "https://api.mrdiscord.repl.co/vityea_info")
-    x.send(null)
-    x.onload = () => {
-        if (x.readyState === x.DONE){
-            document.getElementById("newsUpdates").innerHTML=x.responseText;
-        }
-    }
-
-    x.onerror = () => {
-        document.getElementById("newsUpdates").innerHTML="Error! Failed to get the latest news!";
-    }
-
-}
-
